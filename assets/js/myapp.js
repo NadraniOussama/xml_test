7d136bf57169c24e8256aa47a517ca9ded2f8fce
nadrani oussama <nadranisama@gmail.com>
2020-06-17 08:00:23 +0100

    slight problem with modification and supression

--- assets/js/myapp.js
+++ assets/js/myapp.js
@@ -4,42 +4,10 @@ function openForm(){
 }
 function closeForm(){
 	document.getElementById("addform").style.top = "-3000px"
+  // reset form
 
 }
 
-
-// this is a OOP version and this is everything that relate to our function
-// alert("heyo");
-function Personne(nom , desciption , salary) 
-{
-
-// proprties of the class 
-
-  this.nom = nom;
-  this.description = desciption; 
-  this.salary = salary;
-
-// methodes of the classe 
-  this.salutation = function() 
-  {
-    alert('Bonjour ! Je m\'appelle ' + this.nom + ', description :' + this.description + ' .');
-  };
-
-// setters 
-  this.setnom = function(nom) 
-  {
-  		this.nom = nom;
-  };
-
-  this.setDesciption = function(description)
-  {
-  		this.description = description;
-  };
-
-  this.getName =function(){
-    return this.nom;
-  }
-}
 
 // change html part
 // START boucle for in here
@@ -59,27 +27,44 @@ function get_XML (id){
   return xmlDoc;
 } 
 
-function modifier(id)
+function modifier(id_m)
 { 
   openForm();
+  alert("this is id : == > "+id_m);
+  // get line id 
+  line_id = id_m+"_car";
+  // get xmlDom
+  id_m = 2;
+  xml_dom = get_XML(id_m);
+  //  update form
 
+  document.getElementById("car-name").value = xmlDOM.getElementsByTagName("car")[0].childNodes[0].textContent;
+  document.getElementById("car-model").value = xmlDOM.getElementsByTagName("car")[0].childNodes[1].textContent;
+  document.getElementById("car-date").value = xmlDOM.getElementsByTagName("car")[0].childNodes[2].textContent;
+  document.getElementById("car-bgcolor").value = xmlDOM.getElementsByTagName("car")[0].childNodes[3].textContent;
+  document.getElementById("car-mileage").value = parseInt(xmlDOM.getElementsByTagName("car")[0].childNodes[4].textContent);
 
   document.getElementById('submit').onclick = function(){
-    xmlRowString =  document.getElementById("car-name").value;
+
+    name_car =  document.getElementById("car-name").value;
+    model_car =  document.getElementById("car-model").value;
+    date_car =  document.getElementById("car-date").value;
+    bgcolor_car =  document.getElementById("car-bgcolor").value;
+    mileage_car =  document.getElementById("car-mileage").value;
+
+    xml_str = "<car><make>"+name_car+"</make>";
+    xml_str += "<model>"+ model_car+ "</model>";
+    xml_str += "<date>"+ date_car+ "</date>";
+    xml_str += "<bgcolor>"+bgcolor_car+"</bgcolor>";
+    xml_str += "<mileage>"+mileage_car+"</mileage>";
+
     // nums = id.split('_'); 
-    set_XML(id, xmlRowString);
+    set_XML(id_m, xml_str);
 
     document.getElementById('submit').onclick = function (){ set_form() };
   };
 
 }
-
-// Dim xNode As XmlNode = xDoc.AppendChild(xDoc.CreateElement("xml"))
-
-
-// Dim xAuthor As XmlNode = xNode.AppendChild(xDoc.CreateElement("author"))
-// xAuthor.InnerText = "hey"
-//  all the setters
 
 function set_local()
 {
@@ -121,14 +106,11 @@ function set_form()
   xml_str += "<bgcolor>"+bgcolor_car+"</bgcolor>";
   xml_str += "<mileage>"+mileage_car+"</mileage>";
 
-
-
   xml_str += "</car>"
   //  then save
   set_XML(localStorage.length, xml_str);
   
 }
-// localStorage.clear() // clear
 
 function set_XML(pos, xmlRowString)
 {
@@ -159,12 +141,12 @@ function set_XML(pos, xmlRowString)
 
 function updateHTMLListe()
 {
-  xmlString = "";
+  xmlDOM = "";
   listeHTML = document.querySelector('#liste');
   for(i=0 ; i<localStorage.length ; i++)
   {
     
-    xmlString = get_XML(i);
+    xmlDOM = get_XML(i);
    
     line = document.createElement("tr");
     line.id = i+"_car";
@@ -177,15 +159,12 @@ function updateHTMLListe()
     col6 = document.createElement("td");
     col7 = document.createElement("td");
 
-    col1.innerHTML = xmlString.getElementsByTagName("car")[0].childNodes[0].textContent;
-    col2.innerHTML = xmlString.getElementsByTagName("car")[0].childNodes[1].textContent;
-    // col2.id = i+"_id";
+    col1.innerHTML = xmlDOM.getElementsByTagName("car")[0].childNodes[0].textContent;
+    col2.innerHTML = xmlDOM.getElementsByTagName("car")[0].childNodes[1].textContent;
     col2.style.width = "300px"
-    col3.innerHTML = xmlString.getElementsByTagName("car")[0].childNodes[2].textContent;
-    col4.innerHTML = xmlString.getElementsByTagName("car")[0].childNodes[4].textContent;
-    col5.innerHTML = xmlString.getElementsByTagName("car")[0].childNodes[3].textContent;
-
-
+    col3.innerHTML = xmlDOM.getElementsByTagName("car")[0].childNodes[2].textContent;
+    col4.innerHTML = xmlDOM.getElementsByTagName("car")[0].childNodes[4].textContent;
+    col5.innerHTML = xmlDOM.getElementsByTagName("car")[0].childNodes[3].textContent;
 
         modifierButton = document.createElement("button");
         modifierButton.class = "button primary small";
@@ -211,31 +190,12 @@ function updateHTMLListe()
     line.appendChild(col6);
     line.appendChild(col7);
 
-
-
     listeHTML.append(line);
   }
 }
 
-try{
-  if(window.DOMParser){
-    var parser = new DOMParser() ; 
-    // xmlDoc = parser.parseFormString("<root></root>", "text/xml");
-  }
-  else{
-    xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-    xmlDoc.async=false;
-    xmlDoc.loadXML(localStorageRow);
-  }
-
-}catch (error){
-  alert(error);
-}
 // END shit chat
 
-// var xmlDoc = document.implementation.createDocument(null, "xmltest.xml");
-// var parser = new DOMParser(),
-//   xmlDoc = parser.parseFormString(xml, "text/xml");
 
 updateHTMLListe();
 
